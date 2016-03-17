d00989cf8f727b26e94fed41217ca5997a5c5f1b
think4ff <think4ff@naver.com>
2016-03-17 17:48:29 +0900

    2nd init files

---
+++ bin/EMVchipMonitor.bat
@@ -0,0 +1,3 @@
+rem jar 실행명령
+rem cd c:\tmp
+java EMVchipMonitor
---
+++ bin/emvChipErrExec.bat
@@ -0,0 +1,4 @@
+rem error발견시 실행할 명령 입력.
+rem cd c:\tmp
+rem type EMVchipMonitor.property > newProperty.txt
+type .\bin\EMVchipMonitor.property > .\bin\newProperty.txt
